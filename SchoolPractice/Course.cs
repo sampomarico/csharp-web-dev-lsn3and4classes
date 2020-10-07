852af2c1a931fc7c4b141536c9d833bdfdff910f
Sam <samtpomarico@gmail.com>
2020-10-07 15:51:15 -0500

    First commit. Completed of exercise from lsn 3

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    class Course
+    {
+        public string CourseTitle { get; set; }
+        private Dictionary<string, int> studentsEnrolled = new Dictionary<string, int>();
+        public string CourseDayOfWeek { get; set; }
+
+        public Dictionary<string, int> StudentsEnrolled
+        {
+            get { return studentsEnrolled; }
+            set { studentsEnrolled = value; }
+        }
+
+    }
+}
--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SchoolPractice
 {
@@ -7,6 +8,38 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student sam = new Student();
+
+            sam.Name = "Sam";
+            sam.StudentId = 15029;
+            sam.NumberOfCredits = 1;
+            sam.Gpa = 4.0;
+
+            Student logan = new Student();
+
+            logan.Name = "Logan";
+            logan.StudentId = 321654;
+            logan.NumberOfCredits = 1;
+            logan.Gpa = 4.0;
+
+            Console.WriteLine(sam.Name);
+            Console.WriteLine(sam.StudentId);
+            Console.WriteLine(sam.NumberOfCredits);
+            Console.WriteLine(sam.Gpa);
+
+            Course csharp = new Course();
+
+            csharp.CourseTitle = "Intro to C#";
+            csharp.StudentsEnrolled.Add(sam.Name, sam.StudentId);
+            csharp.StudentsEnrolled.Add(logan.Name, logan.StudentId);
+            csharp.CourseDayOfWeek = "Tuesday and Thursday";
+
+            Console.WriteLine(csharp.CourseTitle);
+            foreach (KeyValuePair<string, int> student in csharp.StudentsEnrolled)
+            {
+                Console.WriteLine(student.Value);
+            }
+            Console.WriteLine(csharp.CourseDayOfWeek);
         }
     }
 }
